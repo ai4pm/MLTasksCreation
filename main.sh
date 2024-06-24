af18b68e66a4877e8cc10cb2d98b20d9c66a3ea9
Teena Sharma <98439325+tsharma12@users.noreply.github.com>
2024-06-23 20:35:21 -0500

    main.sh

--- main.sh
+++ main.sh
@@ -3,8 +3,8 @@
 #SBATCH --account=ACF-UTHSC0001
 #SBATCH --partition=campus
 #SBATCH --qos=campus
-#SBATCH --output=MultiEthnicMachineLearning/o_e_files/BLCA_OS_1YR_BLACK_Protein.o%j
-#SBATCH --error=MultiEthnicMachineLearning/o_e_files/BLCA_OS_1YR_BLACK_Protein.e%j
+#SBATCH --output=BLCA_OS_1YR_BLACK_Protein.o%j
+#SBATCH --error=BLCA_OS_1YR_BLACK_Protein.e%j
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=8
 #SBATCH --time=01-00:00:00
@@ -15,7 +15,7 @@ source activate multiethnic
 
 echo "The environment has been activated."
 
-python MultiEthnicMachineLearning/BLCA_OS_1YR_BLACK_Protein.py BLCA OS 1 BLACK 200
+python BLCA_OS_1YR_BLACK_Protein.py BLCA OS 1 BLACK 200
 
 echo "The execution has been done."
 
